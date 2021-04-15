d7a7dfc5c68def7566cde9f717aaf8aa0783fb3d
JRJ <jrjensen14@gmail.com>
2021-04-14 20:01:25 -0500

    setting up social-network-api

---
+++ server.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Use this to log mongo queries being executed!
+mongoose.set('debug', true);
